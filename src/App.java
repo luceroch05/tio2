346cf9afb46effe2cb245d4e1a7c247d43eac5f0
johncito <johnzorrillx@gmail>
2023-10-30 20:29:49 -0500

    cambie interfaz

--- src/App.java
+++ src/App.java
@@ -1,8 +1,7 @@
 //import java.awt.*;
 //import java.sql.Date;
 import java.awt.event.*;
-import java.awt.Color;
-import java.awt.Font;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
@@ -31,76 +30,78 @@ public class App {
         frame.setVisible(true);
         frame.setLayout(null);
         frame.setLocationRelativeTo(null);
+        frame.setBackground(new Color(247, 239,234));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         // Cierra la aplicación cuando se cierra la ventana        
 
         panelprincipal=new JPanel();
-        panelprincipal.setBounds(0,0,400,600);
+        panelprincipal.setBounds(0,0,medPantalla.width,medPantalla.height);
         panelprincipal.setLayout(null);
         panelprincipal.setBackground(new Color(247, 239,234));
         frame.add(panelprincipal);
 
-        lbl=new JLabel("Ingrese nombre:");
-        lbl.setBounds(50,60,200,30);
+        lbl=new JLabel("Ingrese nombre");
+        lbl.setBounds(medPantalla.width/2 -100, medPantalla.height/2-280,150,30);
         lbl.setFont(f1);
         lbl.setForeground(c1);;
         panelprincipal.add(lbl);
 
         txtnombre=new JTextField();
-        txtnombre.setBounds(50,90,200,30);
+        txtnombre.setBounds(medPantalla.width/2 -150, medPantalla.height/2-240,200,30);
         txtnombre.setBorder(bor1);
         txtnombre.setFont(f1);
         panelprincipal.add(txtnombre);
 
-        lbl=new JLabel("Direccion:");
-        lbl.setBounds(50,130,150,30);
+        lbl=new JLabel("Direccion");
+        lbl.setBounds(medPantalla.width/2 -80, medPantalla.height/2-200,200,30);
         lbl.setForeground(c1);
         lbl.setFont(f1);
 
         panelprincipal.add(lbl);
 
         txtdireccion= new JTextField();
-        txtdireccion.setBounds(50,160,200,30);
+        txtdireccion.setBounds(medPantalla.width/2 -150, medPantalla.height/2-160,200,30);
         txtdireccion.setFont(f1);
         txtdireccion.setBorder(bor1);
         panelprincipal.add(txtdireccion);
 
-        lbl=new JLabel("Teléfono:");
-        lbl.setBounds(50,200,150,30);
+        lbl=new JLabel("Teléfono");
+        lbl.setBounds(medPantalla.width/2 -80, medPantalla.height/2-120,200,30);
         lbl.setFont(f1);
         lbl.setForeground(c1);
         panelprincipal.add(lbl);
 
         txttelefono= new JTextField();
-        txttelefono.setBounds(50,230,200,30);
+        txttelefono.setBounds(medPantalla.width/2 -150, medPantalla.height/2-80,200,30);
         txttelefono.setBorder(bor1);
         panelprincipal.add(txttelefono);
 
-        lbl=new JLabel("DNI:");
-        lbl.setBounds(50,270,200,30);
+        lbl=new JLabel("DNI");
+        lbl.setBounds(medPantalla.width/2 -70, medPantalla.height/2-40,200,30);
         lbl.setFont(f1);
         lbl.setForeground(c1);
         panelprincipal.add(lbl);
 
         txtdni= new JTextField();
-        txtdni.setBounds(50,300,200,30);
+        txtdni.setBounds(medPantalla.width/2 -150, medPantalla.height/2+0,200,30);
+
         txtdni.setBorder(bor1);
         panelprincipal.add(txtdni);
 
-        lbl=new JLabel("Fecha de nacimiento:");
-        lbl.setBounds(50,340,150,30);
+        lbl=new JLabel("Fecha de nacimiento");
+        lbl.setBounds(medPantalla.width/2 -120, medPantalla.height/2+40,200,30);
         lbl.setFont(f1);
         lbl.setForeground(c1);
         panelprincipal.add(lbl);
 
         txtnacimiento= new JTextField();
-        txtnacimiento.setBounds(50,370,200,30);
+        txtnacimiento.setBounds(medPantalla.width/2 -150, medPantalla.height/2+80,200,30);
         txtnacimiento.setBorder(bor1);
         panelprincipal.add(txtnacimiento);
 
 
         btnProcesar=new JButton("Registrar");
-        btnProcesar.setBounds(50,410,200,30);
+        btnProcesar.setBounds(medPantalla.width/2 -150, medPantalla.height/2+200,200,30);
         
         btnProcesar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent i) {
